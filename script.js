71b5a86355720df989218c031a5d0e1ee03365ef
Jamshid_02 <jamshidbeknazarov280@gmail.com>
2023-04-18 18:50:47 +0500

    birinchi yuklash

---
+++ script.js
@@ -0,0 +1,207 @@
+// integer 1
+////let a=parseInt("5000");
+////let b=parseInt(a/100);
+//console.log(b);
+
+// integer 2
+////let a=parseInt("4234");
+////let b=parseInt(a/1000);
+//console.log(b);
+
+// integer 3
+////let a=parseInt("425674");
+////let b=parseInt(a/1024);
+//console.log(b);
+
+// integer 4
+////let a=20;
+////let b=4;
+////let  c=parseInt(a/b)*b;
+//console.log(c);
+
+
+ //integer 5
+////let a=45;
+////let b=4;
+////let  c=parseInt(a/b)*b;
+////let k=parseInt(a-c);
+//console.log(c,k);
+
+// integer 6
+
+////let a=62;
+////let b=parseInt(a/10);
+////let c=parseInt(a%10)
+//console.log(b);
+//console.log(c);
+
+//integer 7
+////let a=56;
+////let b=parseInt(a/10);
+////let c=parseInt(a%10);
+////let d=b+c;
+//console.log(d);
+
+// integer 8
+
+////let a=56;
+////let a=65;
+////let b=parseInt(a/10);
+////let c=parseInt(a%10);
+////let k=c*10+b;
+//console.log(k);
+
+// integer 9
+////let a=468;
+////let b=parseInt(a/100);
+//console.log(b); 
+
+
+// integer 10
+////let a=579;
+////let b=parseInt(a%10);
+////let c=parseInt(a/10)%10;
+//console.log(b,c);
+
+
+// integer 11
+////let a=579;
+////let b=parseInt(a/100);
+////let c=parseInt(a/10)%10;
+////let k=parseInt(a%10);
+////let m=parseInt(b+c+k);
+//console.log(m);
+// integer 12
+////let a=789;
+////let b=parseInt(a%10);
+////let c=parseInt(a/10)%10;
+////let d=parseInt(a/100);
+////let m=b*100+c*10+d;
+//console.log(m);
+// integer 13
+////let a=469;
+////let b=parseInt(a/10)%10;
+////let c=parseInt(a%10);
+////let d=parseInt(a/100);
+////let m=b*100+c*10+d;
+//console.log(m);
+// integer 14
+////let a=956;
+////let b=parseInt(a%10);
+////let c=parseInt(a/100);
+////let d=parseInt(a/10)%10;
+//et f=b*100+c*10+d;
+//console.log(f);
+// integer 15
+////let a=123;
+////let b=parseInt(a/10)%10;
+////let c=parseInt(a/100);
+////let f=parseInt(a%10);
+/////let m=b*100+c*10+f;
+//console.log(m);
+// integer 16
+////let a=123;
+////let b=parseInt(a/100);
+////let c=parseInt(a%10);
+////let d=parseInt(a/10)%10;
+////let m=b*100+c*10+d;
+//console.log(m);
+// integer 17
+////let a=4579;
+////let b=parseInt(a/100)%10;
+//console.log(b);
+// integer 18
+////let a=6789;
+////let b=parseInt(a/1000);
+//console.log(b);
+// integer 19
+////let a=8567;
+////let b=parseInt(a/60);
+//console.log(b);
+// integer 20
+////let a=4590;
+////let b=parseInt(a/3600);
+//console.log(b);
+// integer 21
+////let a=9543;
+////let b=parseInt(a/60);
+//console.log(b);
+// integer 22
+////let a=5600;
+////let m=parseInt(a/60);
+//console.log(a);
+//console.log(m);
+//integer 23
+////let a=6789;
+////let m=parseInt(a/60);
+////let s=parseInt(a/3600);
+//console.log(a,m,s);
+// integer 24 
+////let k=335;
+////let c=parseInt(k%7);
+//console.log(c);
+// integer 25 
+////let k=338;
+////let c=parseInt((k%7)+3)%7;
+//console.log(c);
+//integer 26
+////let k=350;
+////let b=parseInt((k%7)+1)%7;
+//console.log(b);
+//integer 27
+////let k=350;
+////let b=parseInt((k%7)+6)%7;
+//console.log(b);
+//integer 28
+////let k=320;
+////let N=5;
+////let b=parseInt((k%7)+N)%7;
+//console.log(b);
+// integer 29
+////let a=10;
+////let b=35;
+////let c=8;
+////let t=parseInt(a*b);
+////let m=parseInt(c**2);
+////let n=parseInt(t/m);
+////let s=parseInt(t-5*m);
+//console.log(s,n);
+// integer 30
+////let a=1500;
+////let b=parseInt((a/100) +1);
+//console.log(b);
+// boolen5
+////let A=4;
+////let B=-7;
+////let f= A>=0 && B<-2;
+//console.log(f);
+// boolen6
+////let A=2;
+//et B=4;
+////let C=5;
+////let f=A<=B  &&  B<=C;
+//console.log(f);
+
+//boolean22
+//let a=346;
+//let b=parseInt(a/100);
+//let c=parseInt(a/10)%10;
+//let d=parseInt(a%10); 
+//let f= b<c && c<d; 
+//let t= b>c && c>d;
+//console.log(f);
+//console.log(t);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
